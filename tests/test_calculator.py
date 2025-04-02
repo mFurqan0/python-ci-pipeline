c03714fd6a137b738a489a3bd2e4dee97d54afa0
mFurqan0 <furqanbusiness0@gmail.com>
2025-04-02 13:14:29 +0500

    seperately checking flake8 because of conflicting preferences

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,9 +1,12 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 
-from calculator import add, subtract, multiply, divide
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src"))
+)
+
+from calculator import add, subtract, multiply, divide  
 
 
 def test_add():
